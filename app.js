548aeccbfbf128f1f96a2096c43a8ccf223cd51a
Onassis <onassis.sowah@gmail.com>
2022-03-17 15:46:26 +0100

    final

--- app.js
+++ app.js
@@ -9,21 +9,10 @@ class Contact {
 //UI class
 class UI {
     static displayContacts() {
-        const storedContacts = [
-            {
-                name: 'Onassis Sowah',
-                number: '02448473',
-                location: 'accra'
-            },
-            {
-                name: 'Legacy Anyetei',
-                number:'0349498',
-                location: 'accra'  
-            }
+        
+        
 
-        ]
-
-        const contacts = storedContacts;
+        const contacts = Store.getContacts();
 
         contacts.forEach(contact =>  UI.addContactToList(contact));
 
@@ -47,7 +36,17 @@ class UI {
           el.parentElement.parentElement.remove();  
         }
     }
+ static showAlert(message, className){
+ const div = document.createElement('div')
+ div.className = `alert alert-${className} `;
+ div.appendChild(document.createTextNode(message));
+ const container = document.querySelector('.container')
+ const form = document.getElementById('book-form')
+ container.insertBefore(div,form)
+ // dissapear 
+ setTimeout(() => document.querySelector('.alert').remove(), 2000)
 
+ }
 
     static clearFields(){
         document.getElementById('name').value= ''
@@ -57,6 +56,38 @@ class UI {
 }
 
 // Storage Class: handles storage
+class Store {
+    static getContacts(){
+        let contacts;
+        if(localStorage.getItem('contacts')===null){
+            contacts =[]
+
+        } else{
+            contacts =JSON.parse(localStorage.getItem('contacts'));
+        }
+        return contacts;
+    }
+
+    static addContacts(contact){
+        const contacts = Store.getContacts();
+
+        contacts.push(contact)
+
+     localStorage.setItem('contacts', JSON.stringify(contacts) );
+    }
+
+    static removeContacts(location){
+        const contacts = Store.getContacts();
+
+    contacts.forEach((contact, index)=> {
+        if(contact.location == location){
+           contacts.splice(index, 1);
+        }
+        localStorage.setItem('contacts', JSON.stringify(contacts))
+    })
+
+    }
+}
 
 // Event: display contacts
 document.addEventListener('DOMContentLoaded', UI.displayContacts);
@@ -73,20 +104,38 @@ const name = document.getElementById('name').value
 const number = document.getElementById('number').value
 const location = document.getElementById('location').value
 
+//validate
+if (name===''||number===''||location===''){
+   UI.showAlert('please fill in all the fields', 'danger')
+}
+else{
+
 // instantiate a new contact
 const contact = new Contact(name, number, location);
-console.log(contact)
+
 
 // add contact to UI
 UI.addContactToList(contact)
 
+// add book to store
+Store.addContacts(contact)
+
+// show contact added
+UI.showAlert('Contact Added', 'success')
+
 //clear fields
 UI.clearFields()
-})
-//event: remove a book 
+}
+});
+//event: remove a contact 
 document.getElementById('book-list').addEventListener('click', (e) =>{
 
-UI.deletecontact(e.target);
+UI.deletecontact(e.target)
 
+//remove book from store
+Store.removeContacts(e.target.parentElement.previousElementSibling.textContent)
+// show contact deleted
+UI.showAlert('Contact deleted', 'danger')
 })
-   
+
+
